6db37fc186518e2b7539fbe72710d42d9fcae0a8
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-19 14:17:35 -0500

    ShutterSlider fixed

---
+++ scripts/CameraMode.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var background = $SceneBackground
+@onready var aperture_slider = $ApertureSlider
+@onready var shutter_slider = $ShutterSlider
+@onready var iso_slider = $ISOSlider
+
+func _ready():
+	aperture_slider.value_changed.connect(_on_settings_changed)
+	shutter_slider.value_changed.connect(_on_settings_changed)
+	iso_slider.value_changed.connect(_on_settings_changed)
+	
+func _on_settings_changed(_value):
+	var brightness = aperture_slider.value / 50.0
+	background.modulate = Color(brightness, brightness, brightness)
+	
+	var blur = (100 - shutter_slider.value) / 100.0 * 0.02
+	background.material.set_shader_parameter("blur_amount", blur)
